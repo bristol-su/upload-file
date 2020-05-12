a6de32bc2350dc2f306d0a8ff5ee9e42388f1c6e
Toby Twigger <tobytwigger1@gmail.com>
2020-05-12 10:05:32 +0100

    Fix v-modelling

--- resources/js/components/admin/Audience.vue
+++ resources/js/components/admin/Audience.vue
@@ -4,6 +4,7 @@
             :options="options"
             :value="value" 
             @input="$emit('input', $event)"
+            :reduce="obj => obj.value"
         ></v-select>
     </div>
 </template>
@@ -48,18 +49,18 @@
         computed: {
             options() {
                 return this.audience.map(a => {
-                    let text = '';
+                    let label = '';
                     if(a.resource_type === 'user') {
-                        text = a.participant.data.first_name + ' ' + a.participant.data.last_name + ' (' + a.name + ')';
+                        label = a.participant.data.first_name + ' ' + a.participant.data.last_name + ' (' + a.name + ')';
                     }
                     if(a.resource_type === 'group') {
-                        text = a.participant.data.name + ' (' + a.name + ')';
+                        label = a.participant.data.name + ' (' + a.name + ')';
                     }
                     if(a.resource_type === 'role') {
-                        text = a.participant.data.role_name + ' (' + a.name + ')';
+                        label = a.participant.data.role_name + ' (' + a.name + ')';
                     }
                     
-                    return { value: a.id, text: text }
+                    return { value: a.id, label: label }
                 })
             }
         }
