25f94d215f1e44ce1b53e69b9724dceb652d1298
Toby Twigger <tobytwigger1@gmail.com>
2021-09-02 12:01:29 +0100

    Update duplicate route names

--- routes/participant/api.php
+++ routes/participant/api.php
@@ -8,6 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('file/old', [OldFileController::class, 'index']);
 Route::apiResource('file', FileController::class)->parameters(['file' => 'uploadfile_file']);
 Route::prefix('file/{uploadfile_file}')->group(function() {
-    Route::apiResource('comment', CommentController::class)->only(['index', 'store'])->parameters(['comment' => 'uploadfile_comment']);
+    Route::apiResource('comment', CommentController::class, ['as' => 'participant'])->only(['index', 'store'])->parameters(['comment' => 'uploadfile_comment']);
 });
-Route::apiResource('comment', CommentController::class)->only(['update', 'destroy'])->parameters(['comment' => 'uploadfile_comment']);
+Route::apiResource('comment', CommentController::class, ['as' => 'participant'])->only(['update', 'destroy'])->parameters(['comment' => 'uploadfile_comment']);
