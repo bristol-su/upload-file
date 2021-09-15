bae757765763ccf12adc00a441d3f3263649fe9e
Toby Twigger <tobytwigger1@gmail.com>
2021-09-15 17:01:16 +0100

    Add a role buttpon

--- resources/js/components/admin/AdminFiles.vue
+++ resources/js/components/admin/AdminFiles.vue
@@ -35,7 +35,7 @@
                             <span class="sr-only">Download</span>
                         </span>
                 </a>
-                <a href="#" @click.prevent="changeStatus(row)" v-if="canChangeStatus" class="text-success hover:text-success-dark">
+                <a href="#" @click.prevent="changeStatus(row)" v-if="canChangeStatus" class="text-success hover:text-success-dark" role="button">
                     <span>
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
                              content="Change Status" v-tippy="{ arrow: true, animation: 'fade', placement: 'top-start', arrow: true, interactive: true}"
@@ -45,7 +45,7 @@
                     </span>
                     <span class="sr-only">Change Status</span>
                 </a>
-                <a href="#" @click.prevent="showComments(row)" v-if="canSeeComments" class="text-primary hover:text-secondary-dark">
+                <a href="#" @click.prevent="showComments(row)" v-if="canSeeComments" class="text-primary hover:text-secondary-dark" role="button">
                     <span class="flex">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" content="View Comments" v-tippy="{ arrow: true, animation: 'fade', placement: 'top-start', arrow: true, interactive: true}">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
